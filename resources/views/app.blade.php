09a84a47927569d1d810b3489b5e052daf16dbfc
Elias <eliassaulojoslimuyu@hotmail.com>
2025-06-30 22:19:51 -0500

    icono implementado

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('secure_login.png') }}">
     <title>{{ env('APP_NAME') }}</title>
     @stack('styles')
 </head>
