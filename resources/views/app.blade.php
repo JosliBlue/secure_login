53b7a23c15b0ff81c57ead135f70824138da2442
Elias <eliassaulojoslimuyu@hotmail.com>
2025-06-30 22:48:36 -0500

    frontend login implementado

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,15 +4,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" type="image/png" href="{{ asset('secure_login.png') }}">
     <title>{{ env('APP_NAME') }}</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="stylesheet" href="{{ asset('css/css2-google_fonts.css') }}">
+    <script src="{{ asset('js/iconify.min.js') }}"></script>
+    <script src="{{ asset('js/tailwind-3_4_16.js') }}"></script>
+
+    <style>
+        * {
+            font-family: "Inter", sans-serif !important;
+            font-optical-sizing: auto;
+            font-weight: normal;
+            font-style: normal;
+        }
+    </style>
+
     @stack('styles')
+
 </head>
 
 <body>
+
     @yield('content')
+
     @stack('scripts')
+
 </body>
 
 </html>
